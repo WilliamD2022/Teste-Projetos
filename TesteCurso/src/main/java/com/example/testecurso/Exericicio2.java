72788003456824c989e078853bd9fcee9003ba4b
WilliamD2022 <williamcdzx88@gmail.com>
2022-07-16 12:56:08 -0300

    novos exercicios

---
+++ TesteCurso/src/main/java/com/example/testecurso/Exericicio2.java
@@ -0,0 +1,69 @@
+package com.example.testecurso;
+
+import java.util.Scanner;
+
+//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+//mensagem explicativa, conforme exemplos.
+
+//Entrada: Saída:
+//        10
+//        30
+//        SOMA = 40
+//        Entrada: Saída:
+//        -30
+//        10
+//        SOMA = -20
+//        Entrada: Saída:
+//        0
+//        0
+//        SOMA
+
+
+public class Exericicio2 {
+
+    //    public static void main (String[] args){
+//     Scanner sc = new Scanner(System.in);
+//
+//     int  A,B,soma;
+//
+//        A = sc.nextInt();
+//        B = sc.nextInt();
+//
+//        soma = A + B;
+//
+//        System.out.println("SOMA = " +soma);
+//
+//        sc.close();
+//
+//
+//
+//    }
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int A, B, sub;
+//
+//        A = sc.nextInt();
+//        B = sc.nextInt();
+//
+//        sub = (A - B);
+//
+//        System.out.println("SOMA = " + -sub);
+
+
+//    }
+
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int A, B, soma;
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        soma = A+B;
+
+        System.out.println("SOMA = " + soma);
+
+
+
+    }
+}
